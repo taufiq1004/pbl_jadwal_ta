6a8a57d9383aed756b9e75cd92328bf773bf617f
taufiq1004 <taufiqurrahman101010@gmail.com>
2024-04-22 11:13:14 +0700

    commit 3

--- resources/views/landingpages.blade.php
+++ resources/views/landingpages.blade.php
@@ -38,7 +38,7 @@
                 <ul>
                     <li><a href="/#hero" class="active">Home</a></li>
                     <li><a href="/#about">About</a></li>
-                    <li><a href="/#recent-posts">Berita</a></li>
+                    <li><a href="/#recent-posts">Berita</a></li`>
                     <li><a href="/#team">Team</a></li>
                     <li><a href="/#contact">Contact</a></li>
                 </ul>
